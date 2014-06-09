ecb5026ff7071bc1f08020f805fd328aec364a21
zhaojunbo <zhaojunbo@intern.douban.com>
2014-06-09 18:30:58 +0800

    Add script for a binary classes test on validation set

--- cpp/svm_test.py
+++ cpp/svm_test.py
@@ -16,8 +16,11 @@ TRAIN_DATA_PATH = os.path.join(BASE_PATH, 'data_spec_train')
 TEST_DATA_PATH = os.path.join(BASE_PATH, 'data_spec_test')
 MODEL_PATH = os.path.join(BASE_PATH, 'output_sdae')
 FEA_LAYER = -1
-SAVE_OR_LOAD = True
-CROSS_VALIDATION = True
+# Which test want to pick?
+VALIDATE_NUM = 10000
+BINARY_TEST = True # SVM on a part of Va set
+CROSS_VALIDATION = True # SVM on Va set
+SAVE_OR_LOAD = True # SVM on Tr and Te sets
 
 
 def sigmoid(x):
@@ -40,49 +43,28 @@ def load_text_file(filename):
     return np.array(res)
 
 
-def data_dist(labels):
-    GID_adj = range(len(GID))
-    lbl_hist = []
-    lbl_hist.extend([0] * len(GID_adj))
-    for label_elem in labels:
-        try:
-            lbl_hist[GID_adj.index(label_elem)] += 1
-        except:
-            import traceback
-            traceback.print_exc()
-    for gid_elem, lbl_hist_elem in zip(GID_adj, lbl_hist):
-        print '%i: %i' % (gid_elem, lbl_hist_elem)
-
-
-def main():
-    dpark_ctx = dpark.DparkContext('mesos')
-    assert os.path.isdir(BASE_PATH) and os.path.isdir(MODEL_PATH)
-    
-    # Read the weights and bias of SDAE from MODEL_PATH
+def load_ae(ae_model_path):
     print 'Loading the model'
-    print 'Will adopt layer No. %i' % FEA_LAYER
     W = dict()
     b = dict()
-    n_lyr = len(os.listdir(MODEL_PATH)) / 4 # number of layers
+    n_lyr = len(os.listdir(ae_model_path)) / 4 # number of layers
     for _lyr in range(n_lyr):
         fn_name_W = 'ae_layer_' + str(_lyr) + '_W1'
-        fn_name_W = os.path.join(MODEL_PATH, fn_name_W)
+        fn_name_W = os.path.join(ae_model_path, fn_name_W)
         fn_name_b = 'ae_layer_' + str(_lyr) + '_b1'
-        fn_name_b = os.path.join(MODEL_PATH, fn_name_b)
+        fn_name_b = os.path.join(ae_model_path, fn_name_b)
         W[_lyr] = load_text_file(fn_name_W)
         b[_lyr] = load_text_file(fn_name_b)
-    print 'model loading done'
-    
-    # SVM
-    lyr = W.keys()
-    lyr.sort()
-    # TODO Involve the layer aiming at.
-    lyr_last = lyr[FEA_LAYER]
-    lyr = lyr[:FEA_LAYER]
-    lyr.append(lyr_last)
-    # SVM training data
-    svm_data_tr_va = []
-    svm_label_tr_va = []
+    print 'Loading done'
+    return W, b
+
+
+def load_svm_tr_va_data(tr_path, dpark_ctx, para):
+    lyr = para[0]
+    W = para[1]
+    b = para[2]
+    data_tr_va = []
+    label_tr_va = []
     def map_once_tr(str_data):
         _str_data = str_data.split()
         key = float(_str_data[-1])
@@ -93,7 +75,7 @@ def main():
             val = sigmoid(np.dot(W.get(lyr_elem), val) + b.get(lyr_elem))
         return key, val
     vec_rdd = dpark_ctx.textFile(
-                TRAIN_DATA_PATH
+                tr_path
             ).map(
                 map_once_tr
             )
@@ -107,17 +89,23 @@ def main():
         assert all(_tmp_label == tmp_label[0] for _tmp_label in tmp_label)
         tmp_data = np.array([_data_agg_elem[1] for _data_agg_elem in data_agg_elem])
         # TODO tmp_data.shape = (30, 30, 1), why?
-        svm_data_tr_va.append(list(tmp_data.reshape(tmp_data.size,)))
-        svm_label_tr_va.append(int(map_label(tmp_label[0])))
-        if i % 1000 == 0:
-            print 'Finish aggregate %i patch' % i
-        svm_data_tr = svm_data_tr_va[10000:]
-        svm_label_tr = svm_label_tr_va[10000:]
-        svm_data_va = svm_data_tr_va[:10000]
-        svm_label_va = svm_label_tr_va[:10000]
-    # SVM testing data
-    svm_label_te = []
-    svm_data_te = []
+        data_tr_va.append(list(tmp_data.reshape(tmp_data.size,)))
+        label_tr_va.append(int(map_label(tmp_label[0])))
+        #if i % 1000 == 0:
+        #    print 'Finish aggregate %i patch' % i
+        data_tr = data_tr_va[VALIDATE_NUM:]
+        label_tr = label_tr_va[VALIDATE_NUM:]
+        data_va = data_tr_va[:VALIDATE_NUM]
+        label_va = label_tr_va[:VALIDATE_NUM]
+    return data_tr, label_tr, data_va, label_va
+
+
+def load_svm_te_data(te_path, dpark_ctx, para):
+    lyr = para[0]
+    W = para[1]
+    b = para[2]
+    label_te = []
+    data_te = []
     def map_once_te(data):
         key = map_label(int(data[0]))
         val = np.array(data[1][0]).T
@@ -125,37 +113,90 @@ def main():
         for lyr_elem in lyr:
             val = sigmoid(np.dot(W.get(lyr_elem), val) + b.get(lyr_elem))
         val = reduce(lambda x, y: np.append(x, y), val) # TODO order
-        val = val.reshape(val.size,).tolist() # TODO Code is a little too complicated
+        val = val.reshape(val.size,).tolist()
         return key, val
     vec_rdd = dpark_ctx.beansdb(
-            TEST_DATA_PATH
+                te_path
             ).map(
                 map_once_te
             )
     data = vec_rdd.collect()
     for data_elem in data:
-        svm_data_te.append(data_elem[1])
-        svm_label_te.append(int(data_elem[0]))
+        data_te.append(data_elem[1])
+        label_te.append(int(data_elem[0]))
+    return data_te, label_te
+
+
+def data_dist(GID_adjust, labels):
+    lbl_hist = []
+    lbl_hist.extend([0] * len(GID_adjust))
+    for label_elem in labels:
+        try:
+            lbl_hist[GID_adjust.index(label_elem)] += 1
+        except:
+            import traceback
+            traceback.print_exc()
+    for gid_elem, lbl_hist_elem in zip(GID_adjust, lbl_hist):
+        print '%i: %i' % (gid_elem, lbl_hist_elem)
+    return lbl_hist
+
+
+def main():
+    dpark_ctx = dpark.DparkContext('mesos')
+    assert os.path.isdir(BASE_PATH) and os.path.isdir(MODEL_PATH)
+    
+    # Read the weights and bias of SDAE from MODEL_PATH
+    W, b = load_ae(MODEL_PATH)
+
+    # SVM
+    print 'Will adopt layer No. %i' % FEA_LAYER
+    lyr = W.keys()
+    lyr.sort()
+    lyr_last = lyr[FEA_LAYER]
+    lyr = lyr[:FEA_LAYER]
+    lyr.append(lyr_last)
+
+    # SVM training and validating data
+    svm_data_tr, svm_label_tr, svm_data_va, svm_label_va = \
+            load_svm_tr_va_data(TRAIN_DATA_PATH, dpark_ctx, (lyr, W, b))
+    # SVM testing data
+    svm_data_te, svm_label_te = load_svm_te_data(TEST_DATA_PATH, dpark_ctx, (lyr, W, b))
 
     # Process data, view the GID distribution in tr or te sets
     print 'Processing data here.'
+    GID_adjust = range(len(GID)) # GID adjust to 0-13
     print '=' * 100
     print 'Training data distributions:'
-    data_dist(svm_label_tr)
+    tr_hist = data_dist(GID_adjust, svm_label_tr)
     print '=' * 100
     print 'Testing data distributions:'
-    data_dist(svm_label_te)
+    te_hist = data_dist(GID_adjust, svm_label_te)
     print '=' * 100
     print 'Validation data distributions:'
-    data_dist(svm_label_va)
-    # Cross Validation
-    if CROSS_VALIDATION:
+    va_hist = data_dist(GID_adjust, svm_label_va)
+
+    # Binary Test on two classes in validation set
+    if BINARY_TEST:
+        print 'Doing a binary class test using validation set'
+        lbl1 = 7
+        lbl2 = 11
+        svm_data_va_bin = []
+        svm_label_va_bin = []
+        for lbl_elem, data_elem in zip(svm_label_va, svm_data_va):
+            if lbl_elem == lbl1 or lbl_elem == lbl2:
+                svm_label_va_bin.append(lbl_elem)
+                svm_data_va_bin.append(data_elem)
+        print 'Binary class data was prepared.'
+        for svm_c in [0.001, 0.01, 0.1, 1, 10, 100, 1000, 10000]:
+            svm_opt = '-c ' + str(svm_c) + ' -v 5'
+            svm_model = svm.svm_train(svm_label_va, svm_data_va, svm_opt)
+
+    # Cross Validation on whole validation set
+    elif CROSS_VALIDATION:
         print 'SVM model starts cross validating.'
-        del svm_data_tr, svm_data_te, svm_data_tr_va, \
-                svm_label_tr, svm_label_te, svm_label_tr_va
         for svm_c in [0.001, 0.01, 0.1, 1, 10, 100, 1000, 10000]:
             svm_opt = '-c ' + str(svm_c) + ' '
-            for gid_elem, va_hist_elem in zip(GID_adj, va_hist):
+            for gid_elem, va_hist_elem in zip(GID_adjust, va_hist):
                 wgt_tmp = max(va_hist) / float(va_hist_elem)
                 '''
                 if wgt_tmp < 3.0:
@@ -177,37 +218,37 @@ def main():
             svm_opt += '-v 5 -q'
             print svm_opt
             svm_model = svm.svm_train(svm_label_va, svm_data_va, svm_opt)
-        sys.exit(1)
 
-    # SVM running
-    fn_svm = 'svm_model_c0_01_wgt'
-    if SAVE_OR_LOAD: # True
-        print 'SVM model starts training.'
-        svm_opt = '-c 0.01 '
-        for gid_elem, tr_hist_elem in zip(GID_adj, tr_hist):
-            wgt_tmp = max(tr_hist) / float(tr_hist_elem)
-            if wgt_tmp < 3.0:
-                wgt = 1
-            elif wgt_tmp < 10:
-                wgt = 2
-            elif wgt_tmp < 40:
-                wgt = 4
-            else:
-                wgt = 8
-            svm_opt += ('-w' + str(gid_elem) + ' ' + str(wgt) + ' ')
-        print svm_opt
-        svm_model = svm.svm_train(svm_label_tr, svm_data_tr, svm_opt)
-        # save SVM model
-        svm.svm_save_model(fn_svm, svm_model)
-    else: # False
-        print 'SVM model loading.'
-        # load SVM model
-        svm_model = svm.svm_load_model(fn_svm)
-    print 'SVM model training or loading done'
-    p_label, p_acc, p_val = svm.svm_predict(svm_label_te, svm_data_te, svm_model)
-    fid = open('res_tmp.pkl', 'wb')
-    pickle.dump((p_label, p_acc, p_val), fid)
-    fid.close()
+    # SVM running on whole Training / Testing sets
+    else:
+        fn_svm = 'svm_model_c1_wgt'
+        if SAVE_OR_LOAD: # True
+            print 'SVM model starts training.'
+            svm_opt = '-c 1 '
+            for gid_elem, tr_hist_elem in zip(GID_adjust, tr_hist):
+                wgt_tmp = max(tr_hist) / float(tr_hist_elem)
+                if wgt_tmp < 3.0:
+                    wgt = 1
+                elif wgt_tmp < 10:
+                    wgt = 2
+                elif wgt_tmp < 40:
+                    wgt = 4
+                else:
+                    wgt = 8
+                svm_opt += ('-w' + str(gid_elem) + ' ' + str(wgt) + ' ')
+            print svm_opt
+            svm_model = svm.svm_train(svm_label_tr, svm_data_tr, svm_opt)
+            # save SVM model
+            svm.svm_save_model(fn_svm, svm_model)
+        else: # False
+            print 'SVM model loading.'
+            # load SVM model
+            svm_model = svm.svm_load_model(fn_svm)
+        print 'SVM model training or loading done'
+        p_label, p_acc, p_val = svm.svm_predict(svm_label_te, svm_data_te, svm_model)
+        fid = open('res_tmp.pkl', 'wb')
+        pickle.dump((p_label, p_acc, p_val), fid)
+        fid.close()
 
 if __name__ == '__main__':
     main()
